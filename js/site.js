08aa82361b7e88b3a664640066082596c454d6ab
chatiq-rehman <atiq.rehman1105@gmail.com>
2025-08-15 16:14:33 +0500

    Delete js directory

--- js/site.js
+++
@@ -1,6 +0,0 @@
-﻿const toggleBtn = document.getElementById('toggle-btn');
-const sidebar = document.getElementById('sidebar');
-
-toggleBtn.addEventListener('click', () => {
-    sidebar.classList.toggle('active');
-});
